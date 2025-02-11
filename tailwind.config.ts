5ee6c8fa1f232f576eb8705215d2fc43273b3150
DiegoDev <dmilanop28@gmail.com>
2025-02-11 09:53:23 -0400

    :broom:chore: add dependency and default colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary_color: "#101828",
+        secondary_color: "#475467",
+        tertiary_color: "#6941C6",
+        quaternary_color: "#53389E",
+        quinary_color: "#7F56D9",
       },
     },
   },
