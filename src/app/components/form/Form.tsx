775c64e39d80938b52d019820bdbd8e76a54e241
DiegoDev <dmilanop28@gmail.com>
2025-02-11 20:48:44 -0400

    :wrench:feat: add header

---
+++ src/app/components/form/Form.tsx
@@ -0,0 +1,26 @@
+import Button from "../button/Button";
+import { GoQuestion } from "react-icons/go";
+
+
+export default function Form() {
+    return (
+        <form className="flex flex-col items-center gap-4 sm:gap-2 sm:items-start">
+            <div className="flex flex-col gap-4 items-center sm:flex sm:flex-row sm:gap-6">
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    className="text-sm w-60 h-10 px-2 rounded-lg outline-none text-primary_color border-0 sm:w-72"
+                />
+
+                <Button
+                    name="Get started"
+                    className="text-base font-medium text-white bg-quinary_color hover:bg-violet-700 w-32 h-10"
+                />
+            </div>
+
+            <p className="text-sm text-purple-200">
+                We care about your data in our <span className="border-b border-purple-200">privacy policy</span>.
+            </p>
+        </form>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,47 @@
-import Navbar from "./components/navbar/Navbar"
+import Image from "next/image";
+import Form from "./components/form/Form";
+import Navbar from "./components/navbar/Navbar";
+import userOverTimeChart from "@/app/assets/charts/users-over-time.svg";
+import activeUsers from "@/app/assets/charts/active-users.svg";
 
 export default function Home() {
   return (
     <>
-      <header>
+      <header className="flex flex-col gap-10 items-center">
         <Navbar />
         
-        <h1>Home</h1>
+        <section className="flex flex-col items-center py-16 h-[600px] w-[95%] rounded-xl bg-quaternary_color sm:h-[600px] md:w-[85%]">
+            <article className="flex flex-col items-center gap-10">
+              <header className="flex flex-col items-center gap-2 sm:gap-5">
+                <h1 className="text-4xl text-white font-bold sm:text-6xl">Grow your users.</h1>
+
+                <h2 className="text-4xl text-purple-200 font-bold sm:text-6xl">Smarter.</h2>
+              </header>
+
+              <p className="text-sm text-center text-purple-200 font-normal w-[75%] sm:w-[95%] lg:w-[75%] xl:w-[60%] sm:text-lg">
+                Powerful, self-serve product and growth analytics to help you convert, engage, and retain more users.
+                Trusted by over 4,000 startups.
+              </p>
+
+              <footer>
+                <Form/>
+              </footer>
+            </article>
+        </section>
+
+        <section className="flex flex-col items-center lg:flex lg:flex-row lg:justify-center lg:gap-10">
+            <Image
+                alt="User Over Time Chart"
+                src={userOverTimeChart}
+                className="w-auto h-auto lg:w-[50%] lg:h-[50%] 2xl:absolute 2xl:top-[100%] 2xl:w-[75%] 2xl:h-[75%]"
+            />
+
+            <Image
+                alt="User Over Time Chart"
+                src={activeUsers}
+                className="2xl:absolute 2xl:right-52 2xl:top-[140%]"
+            />
+        </section>
       </header>
     </>
   );
