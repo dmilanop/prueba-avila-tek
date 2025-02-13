666ff0435b277e7fdd4733d74f7f17eb8fcd39c2
DiegoDev <dmilanop28@gmail.com>
2025-02-12 23:02:37 -0400

    :wrench:feat: add dropdown information

--- src/app/components/dropdown/Dropdown.tsx
+++ src/app/components/dropdown/Dropdown.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { DropdownProps } from "./interface/interface";
+import DropdownInfo from "./DropdowInfo";
 
 export default function Dropdown({ name, className, open_icon, close_icon, onClick }: DropdownProps) {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -13,10 +14,22 @@ export default function Dropdown({ name, className, open_icon, close_icon, onCli
 
                 {
                     isOpen ? (
-                        <div className="flex flex-col">
+                        <div className="flex flex-col relative">
                             <button onClick={() => setIsOpen(!isOpen)}>
                                 {close_icon}
                             </button>
+
+                            {
+                                name === "Products" && (
+                                    <DropdownInfo className="top-10 right-[-16px] md:right-1 xl:flex xl:flex-row xl:justify-between xl:gap-8 xl:p-5 xl:w-[500px] xl:h-[250px]"/>
+                                )
+                            }
+
+                            {
+                                name === "Resources" && (
+                                    <DropdownInfo className="top-10 right-[-16px] md:left-1 xl:left-1 xl:flex xl:flex-row xl:justify-between xl:gap-8 xl:p-5 xl:w-[500px] xl:h-[250px]"/>
+                                )
+                            }
                         </div>
 
 
